cd36444d273299e5ed49a21a994df930abb62877
FrancoAmbrosini <162723096+FrancoAmbrosini@users.noreply.github.com>
2024-05-17 16:28:16 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,16 @@
 def enumerate_list(list):
-    return list
+    lista=[]
+    for i, elemento in enumerate(list):
+        if elemento!="":
+            lista.append(f'{len(lista)}. {elemento}')
+    return lista
+
+
 
 
 def enumerate_backwards(list):
-    return list
+    lista = []
+    for i, elemento in enumerate(list):
+        if elemento != "":
+            lista.append(f'{len(lista)}. {elemento[::-1]}')
+    return lista

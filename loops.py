ef728dbab5d3752580b1160c7667756d4e4796d2
FrancoAmbrosini <162723096+FrancoAmbrosini@users.noreply.github.com>
2024-05-17 16:27:51 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,39 @@
-def index_of_by_index(word, list, index):
-    return -1
-
-
-def index_of_empty(list):
-    return -1
-
-
 def index_of(word, list):
+    for i , elemento in enumerate(list):
+        if elemento==word:
+            return i
+    return -1
+
+def index_of_by_index(word, list, posicion_salida):
+    for i in range(posicion_salida, len(list)): #hace el loop desde la salida hasta que encuentra la coincidencia
+        if list[i]==word:
+            return i
     return -1
 
 
-def put(word, list):
+
+def index_of_empty(lista):
+    for i in range (len(lista)):
+        if lista[i]=="":
+            return i
     return -1
 
 
-def remove(word, list):
-    return -1
+
+def put(word,list):
+    for i in range (len(list)):
+        if list[i]=="":
+            list[i]=word
+            return i
+    return-1
+
+
+
+def remove(word,list):
+    borrados=0
+    for i in range (len(list)):
+        if list[i]==word:
+            list[i]=""
+            borrados = borrados + 1
+
+    return borrados
